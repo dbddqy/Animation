28a931b35bf5493f989b1f17b30269d1487896b5
Ruqing Zhong <zhongruqing.sun.zongzi@gmail.com>
2019-03-30 02:37:52 +0800

    增加了ConstructAnimationCameraComponent和ApplyAnimationCameraComponent

--- Animation/Animation/Component/ApplyAnimatinoCameraComponent.cs
+++ Animation/Animation/Component/ApplyAnimatinoCameraComponent.cs
@@ -6,15 +6,15 @@ using Rhino.Geometry;
 
 namespace Animation
 {
-    public class AnimationComponent : GH_Component
+    public class ApplyAnimatinoCameraComponent : GH_Component
     {
         /// <summary>
-        /// Initializes a new instance of the AnimationComponent class.
+        /// Initializes a new instance of the PreviewAnimatinoCameraComponent class.
         /// </summary>
-        public AnimationComponent()
-          : base("Animation", "A",
-              "Make animation captruing rhino viewport.",
-              "Animation", "Animation")
+        public ApplyAnimatinoCameraComponent()
+          : base("ApplyAnimatinoCamera", "AAC",
+              "Apply animation camera in active viewport",
+              "Animation", "AnimationCamera")
         {
         }
 
@@ -23,8 +23,7 @@ namespace Animation
         /// </summary>
         protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
         {
-            pManager.AddGeometryParameter("Geometry", "G", "The geometry to be animated.", GH_ParamAccess.item);
-            //pManager.AddParameter();
+            pManager.AddGenericParameter("AnimationCamera", "AC", "Input camera information", GH_ParamAccess.item);
         }
 
         /// <summary>
@@ -40,6 +39,10 @@ namespace Animation
         /// <param name="DA">The DA object is used to retrieve from inputs and store in outputs.</param>
         protected override void SolveInstance(IGH_DataAccess DA)
         {
+            AnimationCamera animationCamera = AnimationCamera.Default;
+            DA.GetData(0, ref animationCamera);
+
+            animationCamera.SetAimationCamera();
         }
 
         /// <summary>
@@ -60,7 +63,7 @@ namespace Animation
         /// </summary>
         public override Guid ComponentGuid
         {
-            get { return new Guid("6e29f6e5-b6e6-4d11-b1b0-7ad74e3ca5a7"); }
+            get { return new Guid("61b14d7b-a326-4024-9c6c-bbd1e25a6701"); }
         }
     }
 }
